9c78123fac39df9a84c81506f833acd5b1ac13a4
Josh Dewhurst <108956371+joshdewhurst@users.noreply.github.com>
2023-04-26 21:38:07 -0700

    Update main.java
    Add a student

--- main.java
+++ main.java
@@ -85,5 +85,21 @@ public class StudentManagementSystem {
         
         Scanner scanner = new Scanner(System.in);
         int choice;
+        do {
+            System.out.println("1. Add student\n2. Delete student\n3. Update student marks\n4. View student data\n5. Calculate average marks\n6. Exit");
+            System.out.print("Enter your choice: ");
+            choice = scanner.nextInt();
+            switch (choice) {
+                case 1:
+                    System.out.print("Enter student name: ");
+                    String name = scanner.next();
+                    System.out.print("Enter roll number: ");
+                    int rollNumber = scanner.nextInt();
+                    System.out.print("Enter marks: ");
+                    float marks = scanner.nextFloat();
+                    database.addStudent(new Student(name, rollNumber, marks));
+                    break;
+            } }
+
     }
 }
