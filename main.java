90c936abd62f90f78c45fc391051e65654846e6a
Josh Dewhurst <108956371+joshdewhurst@users.noreply.github.com>
2023-04-26 21:20:41 -0700

    Update main.java
    Adding a public class for student

--- main.java
+++ main.java
@@ -4,3 +4,9 @@ class Student {
     private String name;
     private int rollNumber;
     private float marks;
+    
+    public Student(String name, int rollNumber, float marks) {
+        this.name = name;
+        this.rollNumber = rollNumber;
+        this.marks = marks;
+    }
